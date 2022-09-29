3fb00b3d67436890a46d437f51ebe28662c6a2ff
Deepak Sharma <sharmadeepak0939@gmail.com>
2022-09-29 10:53:43 +0530

    first commit

---
+++ game.js
@@ -0,0 +1,6 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+function nextSequence(){
+    var randomNumber = Math.floor((Math.random()*4));
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+}
